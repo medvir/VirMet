b410e668ed93bb5734fd455d724ce2eadb6b827a
ozagordi <osvaldo.zagordi+git@gmail.com>
2013-07-03 16:21:24 +0200

    The pipeline now uses seqtk to remove short reads and trim, then prinseq mainly for the low complexity ones, then deconseq, fr-hit and list_organism.

--- prindefr.sh
+++ prindefr.sh
@@ -11,26 +11,41 @@ MATCH=75
 
 
 prinseq() { /usr/local/bin/prinseq-lite.pl "$@"; }
-#fr-hit() { /home/ozagordi/Projects/3rd_party_SW/fr-hit-v0.7-x86_64/fr-hit "$@"; }
 deconseq() { /home/ozagordi/Dropbox/Software/VirMet/deconseq.pl "$@"; }
-list_organisms() { /home/ozagordi/Dropbox/Software/VirMet/list_organisms.py "$@"; }
+list_organisms() { /home/ozagordi/Dropbox/Software/VirMet/list_organisms.py\
+	 "$@"; }
 
 # print the number of reads to analyze
 echo 'Reads to analyze'
 echo `wc -l $FILEIN | cut -f 1 -d " "` / 4 | bc
 
-# cleaning with prinseq
-prinseq -fastq $FILEIN -derep 1 -exact_only -lc_method entropy -lc_threshold 70 -graph_data -graph_stats ld,gc,qd,de -log prinseq.log -min_len 75 -min_qual_mean 20 -ns_max_p 25 -out_good ./good -out_bad ./bad
+echo `date`
+echo 'cleaning with seqtk'
+seqtk seq -L 75 $FILEIN | seqtk trimfq - > intermediate.fastq
 
-# decontaminate from human and bacterial with deconseq
-deconseq -f good.fastq -dbs hsref,bact -id $DEC_OUT_NAME -keep_tmp_files &> deconseq.log
+echo `date`
+echo 'cleaning with prinseq'
+prinseq -fastq intermediate.fastq -lc_method entropy -lc_threshold 70 \
+	-log prinseq.log -min_qual_mean 20 -ns_max_p 25 -out_good ./good -out_bad ./bad
+rm intermediate.fastq
+
+echo `date`
+echo 'decontaminating from human and bacterial with deconseq'
+deconseq -f good.fastq -dbs hsref,bact -id $DEC_OUT_NAME -keep_tmp_files &> \
+	deconseq.log
 
 # convert fastq to fasta
-CONV_STR="from Bio import SeqIO; rs = SeqIO.convert('${DEC_OUT_NAME}_clean.fq', 'fastq', '$FASTAFILE', 'fasta'); print str(rs) + ' reads converted'"
+CONV_STR="from Bio import SeqIO; rs = SeqIO.convert('${DEC_OUT_NAME}_clean.fq',\
+	 'fastq', '$FASTAFILE', 'fasta'); print str(rs) + ' reads converted'"
 python -c "$CONV_STR"
 
-# run fr-hit
-fr-hit -T 28 -r 1 -k $KMER -g $GLOBAL -m $MATCH -o clean-r_1-k_${KMER}-m_${MATCH}-global_${GLOBAL}.tsv -d $DB -a $FASTAFILE &> fr-hit.log
+echo `date`
+echo 'running fr-hit'
+fr-hit -T 28 -r 1 -k $KMER -g $GLOBAL -m $MATCH -o \
+	clean-r_1-k_${KMER}-m_${MATCH}-global_${GLOBAL}.tsv -d $DB -a $FASTAFILE \
+		&> fr-hit.log
 
-# list organisms in a csv file
-list_organisms clean-r_1-k_${KMER}-m_${MATCH}-global_${GLOBAL}.tsv > orgs_list.csv 2> all_reads_id_orgs.log
+echo `date`
+echo 'listing organisms'
+list_organisms clean-r_1-k_${KMER}-m_${MATCH}-global_${GLOBAL}.tsv > \
+	 orgs_list.csv 2> all_reads_id_orgs.log
