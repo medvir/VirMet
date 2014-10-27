fbb8a7b593038aac8e81497ea489419c555e3057
ozagordi <osvaldo.zagordi+git@gmail.com>
2014-10-27 13:15:13 +0100

    Nucleotide (from nuccore) and protein (only refseq) of viral origin are downloaded and blast db is created.

--- fetch_viral_genomes.py
+++ fetch_viral_genomes.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.4
 # !/opt/python2.7/bin/python2.7
-'''Search and download complete viral genomes from NCBI with edirect tools.
-Check previously downloaded sequences and manually picked in file
-picked_seqs.txt.
+'''Search and download complete viral genomes or refseq proteins from NCBI with
+edirect tools. Check previously downloaded sequences and manually picked in
+file picked_seqs.txt.
 '''
 import sys
 import os
 import re
-from Bio import Entrez
-
-Entrez.email = 'osvaldo.zagordi@uzh.ch'
-
+import datetime
 
 def run_child(exe_name, arg_string):
     '''use subrocess to run an external program with arguments'''
@@ -29,7 +26,18 @@ def run_child(exe_name, arg_string):
 
     return retcode
 
-c_code = run_child('econtact', '-email \"%s\"' % Entrez.email)
+Entrez_email = 'osvaldo.zagordi@uzh.ch'
+c_code = run_child('econtact', '-email \"%s\"' % Entrez_email)
+
+try:
+    db_type = sys.argv[1]
+except KeyError as ee:
+    sys.exit('usage: %s db_type [nuccore, protein]' % sys.argv[0])
+
+if db_type not in ['nuccore', 'protein']:
+    sys.exit('db_type: %s, must be nuccore or protein' % db_type)
+
+os.chdir('/data/databases/viral_%s' % db_type)
 
 print('Parsing sequences already present')
 p_code = run_child('grep',
@@ -61,29 +69,45 @@ txids = {
     '12429': 'unclassified viruses'
     }
 for txid, v in txids.items():
-    search_text = "txid%s [orgn] " % txid + \
-                  "AND \\\"complete genome\\\" [Title] " + \
-                  "NOT txid131567 [orgn]"
-    s_code = run_child('esearch',
-                       '-db nucleotide -query \"%s\" > vir_search_%s' \
-                       % (search_text, txid))
+    if db_type == 'nuccore':
+        search_text = "txid%s [orgn] " % txid + \
+                      "AND \\\"complete genome\\\" [Title] " + \
+                      "NOT txid131567 [orgn]"
 
-# get the number of hits
+    elif db_type == 'protein':
+        search_text = "refseq [filter] AND txid%s [orgn] " % txid + \
+                      "NOT txid131567 [orgn]"
+
+    s_code = run_child('esearch',
+                       '-db %s -query \"%s\" > vir_search_%s'
+                       % (db_type, search_text, txid))
+
+# get the number of hits per taxa
 count = 0
 for txid, v in txids.items():
     for l in open('vir_search_%s' % txid):
         if l.lstrip().startswith('<Count'):
             ch = int(re.search('Count>(\d+)</Count', l).group(1))
-            print('Found %d %s' % (ch, v))
+            print('Found %d hits from %s' % (ch, v))
             count += ch
             break
 print('Summing these, NCBI search returned %d hits' % count)
 
-search_text = "txid10239 [orgn] " + \
-              "AND \\\"complete genome\\\" [Title] " + \
-              "NOT txid131567 [orgn]"
+
+# Now run the search on the whole virus branch
+txid = '10239'
+if db_type == 'nuccore':
+    search_text = "txid%s [orgn] " % txid + \
+                  "AND \\\"complete genome\\\" [Title] " + \
+                  "NOT txid131567 [orgn]"
+
+elif db_type == 'protein':
+    search_text = "refseq [filter] AND txid%s [orgn] " % txid + \
+                  "NOT txid131567 [orgn]"
+
 s_code = run_child('esearch',
-                   '-db nucleotide -query \"%s\" > vir_search' % search_text)
+                   '-db %s -query \"%s\" > vir_search'
+                   % (db_type, search_text))
 
 for l in open('vir_search'):
     if l.lstrip().startswith('<Count'):
@@ -94,7 +118,7 @@ try:
     os.remove('tmp.dmp')
 except OSError:
     pass
-    
+
 print('Saving Gi <-> TaxId <-> Acc relationship of the search')
 s_code = run_child('efetch',
                    '-format docsum < vir_search | ' +
@@ -109,29 +133,31 @@ print('Saving accession numbers')
 s_code = run_child('cut', '-f 3 tmp.dmp > viral_search_NCBI_acc.txt')
 os.remove('tmp.dmp')
 
-print('Now the manually picked sequences')
 id_file = 'picked_seqs.txt'
+print('Now the manually picked sequences from file:', id_file)
+
 try:
     picked_ids = [line.strip().split()[0] for line in open(id_file)]
 except IOError:
     picked_ids = []
 ids_count = len(picked_ids)
-print ('%d sequences listed in %s file' % (ids_count, id_file))
+print ('{} sequences listed in {} file'.format(ids_count, id_file))
 
+# ids to add are the union of picked plus those in ncbi minus those present
 to_add = set(picked_ids) | set(viral_ids)
 to_add = to_add - set(old_ids)
 
-print('%d sequences must be added' % len(to_add))
+print('{} sequences must be added'.format(len(to_add)))
 
 if to_add:
-    print('adding the missing %d' % len(to_add))
+    print('adding the missing {}'.format(len(to_add)))
     print('first to fasta')
     s_code = run_child('efetch',
-                       '-db nuccore -id ' + ','.join(to_add) +
+                       '-db %s -id ' % db_type + ','.join(to_add) +
                        ' -format fasta >> viral_database.fasta')
     print('then to taxonomy')
     s_code = run_child('efetch',
-                       '-db nuccore -id ' + ','.join(to_add) +
+                       '-db %s -id ' % db_type + ','.join(to_add) +
                        ' -format docsum | \
                         xtract -pattern DocumentSummary -element Gi TaxId \
                         >> viral_gi_taxid.dmp')
@@ -139,11 +165,18 @@ else:
     print('No new sequences to add')
 os.remove('vir_search')
 
-# TODO When picked_seqs are present, they must be added to viral_gi_taxid.dmp
+# update blast database
+cml_str = 'makeblastdb -in viral_database.fasta -dbtype '
+if db_type == 'nuccore':
+    cml_str += 'nucl '
+else:
+    cml_str += 'prot '
 
-# TODO update blast database
-'''
-makeblastdb -in viral_database.fasta -dbtype nucl -hash_index
--title "Viral database 35045 sequences 9th April 2014" -out viral_db
--logfile blast.log -parse_seqids -taxid_map viral_gi_taxid.dmp
-'''
+tl = len(to_add) + len(set(old_ids))
+dt = datetime.date.today().isoformat()
+cml_str += "-hash_index \
+-title \"Viral database {} sequences {}\" \
+-out viral_db \
+-logfile blast.log -parse_seqids -taxid_map viral_gi_taxid.dmp".format(tl, dt)
+
+run_child(cml_str)
