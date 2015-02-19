bf50aa0e7bc323f2d60d5041eb43aaaf54232d7e
ozagordi <osvaldo.zagordi+git@gmail.com>
2015-02-19 16:15:11 +0100

    Version 0.3.4 now takes viral database in /data/databases/viral_nuccore

--- viral_blast.sh
+++ viral_blast.sh
@@ -43,7 +43,7 @@ elif [[ $HOSTNAME == "virologysrv04.uzh.ch" ]];
 	XARGS_THREAD=0
 fi
 seq 0 $((NPROC-1)) | xargs -P $XARGS_THREAD -I {} blastn -task megablast \
-	    -query splitted_clean_{}.fasta -db /data/databases/viral_db \
+	    -query splitted_clean_{}.fasta -db /data/databases/viral_nuccore/viral_db \
 		-out tmp_{}.tsv \
 		-outfmt '6 qseqid sseqid sscinames stitle pident qcovs score length mismatch gapopen qstart qend sstart send staxids'
 echo ''
