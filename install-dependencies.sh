d47cacb629eb183bdabd78457f1cb65440453366
ozagordi <osvaldo.zagordi+git@gmail.com>
2015-10-14 16:55:08 +0200

    Installing edirect

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update -qq
-sudo apt-get install -qq -y curl build-essential ncurses-dev byacc zlib1g-dev python-dev git cmake samtools ncbi-blast+
-
-cd ~/
-curl ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.zip -O
-unzip edirect.zip
-export PATH=~/edirect:$PATH
+#  NCBI edirect tools
+wget ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/edirect.tar.gz -O /tmp/edirect.tar.gz
+tar -xvfz /tmp/edirect.tar.gz
