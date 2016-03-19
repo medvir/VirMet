18e87b5540c0e71780cc7a0c0d48dd31c9cef508
ozagordi <osvaldo.zagordi+git@gmail.com>
2016-03-19 15:55:57 +0100

    Working on testing

--- virmet/tests/test_ext_tools.py
+++ virmet/tests/test_ext_tools.py
@@ -4,5 +4,24 @@ import sys
 import unittest
 import subprocess
 
-if __name__ == '__main__':
-    unittest.main()
+virmet_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.sys.path.insert(1, virmet_dir)
+mod = __import__('virmet')
+sys.modules["virmet"] = mod
+
+from virmet.common import run_child
+
+
+class Testbwa(unittest.TestCase):
+
+    def setUp(self):
+        self.remote_1 = 'ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/gencode.v24.primary_assembly.annotation.gtf.gz'
+        self.remote_2 = 'ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/_README.TXT'
+
+    def test_help(self):
+        out = run_child('bwa', 'index')
+        l = len(out.split('\n'))
+        self.assertGreater(l, 6)
+        out = run_child('bwa', 'mem')
+        l = len(out.split('\n'))
+        self.assertGreater(l, 20)
