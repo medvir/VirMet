2d5b70286916990b9cb2fbdf8ee0cd750ad56543
ozagordi <osvaldo.zagordi+git@gmail.com>
2016-05-06 12:38:45 +0200

    Package VirMet with setuptools plus minor fixes
    `setup.py` was added such that the users can install the packages
    with `python setup.py install`. More instructions for conda
    might be added in the future. The structure was changed as
    now `bin/virmet` only contains the import and call to
    the main function in `virmet/__main__.py`. Without installing
    the package, the user can call `path_to_dir/bin/virmet`to run it.
    At the same time, the entry point defined in `setup.py` allows
    the user to have it on their path after installation.
    
    Since we are using `assing` pandas>=0.16 is required and added to
    `environment.yml`.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+setup(
+    name = "VirMet",
+    version = "1.0rc1",
+    packages = find_packages(),
+    scripts = ['bin/virmet'],
+
+    entry_points={
+        'console_scripts': [
+            'virmet = virmet.__main__:main'
+        ]
+#        'gui_scripts': [
+#            'baz = my_package_gui:start_func',
+#        ]
+    },
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        'hello': ['*.msg'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Osvaldo Zagordi",
+    author_email = "firstname.lastname@gmail.com",
+    description = "Viral metagenomics in clinical applications",
+    license = "MIT",
+    download_url = "http://github.com/ozagordi/VirMet",
+    url = "http://virmet.readthedocs.io/en/latest/",
+    long_description = '''
+    Set of tools for the analysis of sequencing data to identify and characterize
+    the viral fraction in metagenomic samples, especially in the clinical setting.
+    '''
+    # could also include long_description, download_url, classifiers, etc.
+)
